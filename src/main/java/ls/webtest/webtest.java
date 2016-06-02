77b208f21f39751e81d193c091594e062bbee5f6
cyberwh <josephlee0717@qq.com>
2016-06-02 15:48:03 +0800

    Add java class

---
+++ src/main/java/ls/webtest/webtest.java
@@ -0,0 +1,64 @@
+package ls.webtest;
+import java.math.*;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import scala.Math;
+
+public class webtest {
+	public static void main(String[] args){
+		
+		String queueIDList = genQueueID();
+		String getReturnFee =  getReturnFee();
+		String processReturnFee= processReturnFee();
+		System.out.println(getReturnFee);		
+	}
+	//IDÀ„∑®
+	public static String genQueueID(){
+		/*
+		 * Every time, the customer create a record of pay action, I get paynumber , and count of ID ,
+		 * Mak up ID list use userid , serial number and date ( userid + serial number + date).   
+		 * and input mix queueID into lineup table ,input others into shalllineup table.
+		*/		
+		double paynumber = 576;
+		//Count of ID
+		double dCount = Math.floor(paynumber/100);
+		int s1 = (int)dCount;
+		String userid = "1100010001";
+		Date dt = new Date();
+	    SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");   
+	    String today = sdf.format(dt);
+	    DecimalFormat df = new DecimalFormat("0000");
+	    
+		Map rtn = new HashMap();
+		for(int i = 0 ; i< s1 ; i++){
+			rtn.put("key"+String.valueOf(i+1), userid+df.format(i+1).toString()+today);
+		}
+		//input every QueueIDs into 
+		return rtn.toString();
+	}
+	
+	public static String getReturnFee(){
+		//Total of customer consume
+		double consumeTotal = 1453.87;
+		//The customer current consume in pay table
+		double curConsume = 432.50;
+		//The expression is: ((consumeTotal mod 100) + curConsume) /100 
+		double rtn = ((consumeTotal%100) + curConsume)/100;
+		DecimalFormat df = new java.text.DecimalFormat("#.00");
+		String rtnStr = df.format(rtn);
+		return String.valueOf(rtnStr);
+		
+		//The SQL expression as this below:
+		//SELECT ((consumeTotal%100)+curConsume)/100
+	}
+	
+	//ReturnFee 
+	public static String processReturnFee(){
+		
+		return "";
+	}
+}
